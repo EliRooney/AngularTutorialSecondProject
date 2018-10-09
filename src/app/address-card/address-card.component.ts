21a84a037ca3c21b34fde3f432d7ad089b6138c7
Eli Rooney <31978743+EliRooney@users.noreply.github.com>
2018-10-09 16:16:51 -0400

    Add files via upload

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-address-card',
@@ -8,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class AddressCardComponent implements OnInit {
 	
 	user: any;
+	@Input('name') userName: string;
 
   constructor() {
+ 
+   }
+
+  ngOnInit() {
   	this.user = {
-		name: 'Foo Bar',
+		name: this.userName,
 		title: 'Software Developer',
 		address: '1234 Main St, City, State, 10010',
 		phone: [
@@ -22,9 +27,6 @@ export class AddressCardComponent implements OnInit {
 			'416-036-1210'
 			]
 		};
-   }
-
-  ngOnInit() {
-  }
+  	}
 
 }
